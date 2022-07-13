7b23eeb15180a30de3cdf7280d1b40785776f281
Isaac Newton Tingo <newtone480@gmail.com>
2022-07-13 13:57:13 +0300

    Secure uri

--- config/db.js
+++ config/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose
-  .connect(
-    "mongodb+srv://IsaacNewtonTingo:Icui4cu1998@cluster0.lthcd.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("DB connected");
   })
