5495047558a65862a1ae80f66b43ff779724fe64
Isaac Newton Tingo <newtone480@gmail.com>
2022-07-13 12:46:48 +0300

    Cors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-
+const cors = require("cors");
 const port = process.env.PORT || 3000;
 
 const bodyParser = require("body-parser").json;
@@ -13,6 +13,7 @@ app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
+app.use(cors());
 const UserRouter = require("./api/user");
 
 app.use("/users", UserRouter);
