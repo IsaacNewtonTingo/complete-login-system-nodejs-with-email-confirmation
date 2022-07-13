876aadfb0cab3c39e3c7c8c2f5b899fc44dd5409
Isaac Newton Tingo <newtone480@gmail.com>
2022-07-13 11:51:38 +0300

    Initial commit

---
+++ api/user.js
@@ -0,0 +1,145 @@
+const express = require("express");
+const router = express.Router();
+
+const User = require("../models/user");
+
+const bcrypt = require("bcrypt");
+
+router.post("/signup", (req, res) => {
+  let { name, email, password, dateOfBirth } = req.body;
+  name = name.trim();
+  email = email.trim();
+  password = password.trim();
+  dateOfBirth = dateOfBirth.trim();
+
+  if (name == "" || email == "" || password == "" || dateOfBirth == "") {
+    res.json({
+      status: "Failed",
+      message: "All fields are required",
+    });
+  } else if (!/^[a-zA-Z ]*$/.test(name)) {
+    res.json({
+      status: "Failed",
+      message: "Invalid name format",
+    });
+  } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+    res.json({
+      status: "Failed",
+      message: "Invalid email",
+    });
+  } else if (!new Date(dateOfBirth).getTime()) {
+    res.json({
+      status: "Failed",
+      message: "Invalid date of birth format",
+    });
+  } else if (password.length < 8) {
+    res.json({
+      status: "Failed",
+      message: "Password is too short",
+    });
+  } else {
+    User.find({ email })
+      .then((result) => {
+        if (result.length) {
+          res.json({
+            status: "Failed",
+            message: "User with the given email already exists",
+          });
+        } else {
+          const salt = 10;
+          bcrypt
+            .hash(password, salt)
+            .then((hashedPassword) => {
+              const newUser = new User({
+                name,
+                email,
+                password: hashedPassword,
+                dateOfBirth,
+              });
+              newUser
+                .save()
+                .then((result) => {
+                  res.json({
+                    status: "Success",
+                    message: "Account created successfully",
+                    data: result,
+                  });
+                })
+                .catch((err) => {
+                  res.json({
+                    status: "Failed",
+                    message: "Error occured while creating account",
+                  });
+                });
+            })
+            .catch((err) => {
+              res.json({
+                status: "Failed",
+                message: "Error occured while hashing password",
+              });
+            });
+        }
+      })
+      .catch((err) => {
+        res.json({
+          status: "Failed",
+          message: "Erro occured when checking email",
+        });
+      });
+  }
+});
+
+router.post("/signin", (req, res) => {
+  let { email, password } = req.body;
+  email = email.trim();
+  password = password.trim();
+
+  if (email == "" || password == "") {
+    res.json({
+      status: "Failed",
+      message: "All fields are required",
+    });
+  } else {
+    User.find({ email })
+      .then((data) => {
+        if (data.length) {
+          const hashedPassword = data[0].password;
+          bcrypt
+            .compare(password, hashedPassword)
+            .then((result) => {
+              if (result) {
+                res.json({
+                  status: "Success",
+                  message: "Signin successfull",
+                  data: data,
+                });
+              } else {
+                res.json({
+                  status: "Failed",
+                  message: "Invalid password",
+                });
+              }
+            })
+            .catch((err) => {
+              res.json({
+                status: "Failed",
+                message: "Error occured while comparing passwords",
+              });
+            });
+        } else {
+          res.json({
+            status: "Failed",
+            message: "Invalid credentials entered",
+          });
+        }
+      })
+      .catch((err) => {
+        res.json({
+          status: "Failed",
+          message: "Error occured checking existing user",
+        });
+      });
+  }
+});
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const bodyParser = require("body-parser").json;
+app.use(bodyParser());
+
+require("./config/db");
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+const UserRouter = require("./api/user");
+
+app.use("/users", UserRouter);
