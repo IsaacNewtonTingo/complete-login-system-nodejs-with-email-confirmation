b827d15702b569f075b10c69d8bb452aa11a795d
Isaac Newton Tingo <newtone480@gmail.com>
2022-07-14 19:23:53 +0300

    fix

--- api/user.js
+++ api/user.js
@@ -19,9 +19,6 @@ const currentUrl = process.env.NODE_ENV ? development : production;
 
 let transporter = nodemailer.createTransport({
   service: "gmail",
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
   auth: {
     user: process.env.AUTH_EMAIL,
     pass: process.env.AUTH_PASS,
